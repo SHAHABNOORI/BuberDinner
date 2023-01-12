0f839c34459662eed252f1a12b0d597be2f6a74b
s_noori <s_noori@SHL-34>
2023-01-13 01:28:18 +0330

    Implemented JWT

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,14 +1,33 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService: IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login( string email, string password)
     {
+        
         return new AuthenticationResult(Guid.NewGuid(), "firstName", "lastName", email, "token");
     }
 
     public AuthenticationResult Register(string firstName, string lastName, string email, string password)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        // Check if user already exists
+
+        // Create user (generate unique Id)
+        Guid userId = Guid.NewGuid();
+
+        // Create JWT token
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+
+        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, token);
     }
 }
